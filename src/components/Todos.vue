74da143a6858808a39357798595600ee6d4a4c94
Avinash J Giri <avingiri@in.ibm.com>
2022-02-12 14:19:09 +0530

    firstversion

---
+++ src/components/Todos.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h3>Todos</h3>
+    <div class="todos">
+      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+        {{ todo.title }}
+        <span @click="deleteTodo(todo.id)" class="x">X</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Todos',
+  methods: {
+    ...mapActions(['fetchTodos', 'deleteTodo'])
+  },
+  computed: mapGetters(['allTodos']),
+  mounted(){
+    this.fetchTodos();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+.x {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: red;
+  cursor: pointer;
+}
+.x:hover {
+  font-size: 20px;
+}
+</style>
